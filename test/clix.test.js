2d5893cd4b4f505714a1e44e0117426f817f0c32
S.Watanabe <ocelot2828@gmail.com>
2024-07-19 16:49:27 +0900

    fix: remove clix.test.js

--- test/clix.test.js
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-import { useAnimation } from '../src/use-animation'
-import styles from './test_styles.module.css'
-
-test(() => {
-  expect(useAnimation(styles.base, [styles.initial]).toBe(styles.initial))
-})
